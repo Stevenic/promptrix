6397677dbdaeb180bc83dc676e68f9d5c6f511e8
Steven Ickman <stevenic@microsoft.com>
2023-05-27 13:40:49 -0700

    100% coverage for TextSection

--- src/TextSection.spec.ts
+++ src/TextSection.spec.ts
@@ -45,6 +45,15 @@ describe("TextSection", () => {
             assert.equal(rendered.length, 2);
             assert.equal(rendered.tooLong, true);
         });
+
+        it("should support multiple message render calls", async () => {
+            const section = new TextSection("Hello World", "user");
+            const rendered1 = await section.renderAsMessages(memory, functions, tokenizer, 1);
+            assert.deepEqual(rendered1.output, [{ role: "user", content: "Hello World" }]);
+
+            const rendered2 = await section.renderAsMessages(memory, functions, tokenizer, 1);
+            assert.deepEqual(rendered2.output, [{ role: "user", content: "Hello World" }]);
+        });
     });
 
     describe("renderAsText", () => {
@@ -62,6 +71,15 @@ describe("TextSection", () => {
             assert.equal(rendered.output, "Hello World");
             assert.equal(rendered.length, 2);
             assert.equal(rendered.tooLong, true);
+        });
+
+        it("should support multiple text render calls", async () => {
+            const section = new TextSection("Hello World", "user");
+            const rendered1 = await section.renderAsText(memory, functions, tokenizer, 1);
+            assert.equal(rendered1.output, "Hello World");
+
+            const rendered2 = await section.renderAsText(memory, functions, tokenizer, 1);
+            assert.equal(rendered2.output, "Hello World");
         });
     });
 });
