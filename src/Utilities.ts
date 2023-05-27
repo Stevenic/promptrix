fe511c48e3e78a70695e16b220adef4d57d4f832
Steven Ickman <stevenic@microsoft.com>
2023-05-27 10:56:46 -0700

    Improved Utilities.toString() and added unit tests

--- src/Utilities.ts
+++ src/Utilities.ts
@@ -4,13 +4,17 @@ import { stringify } from "yaml";
 export class Utilities {
     public static toString(tokenizer: Tokenizer, value: any): string {
         if (typeof value === "object") {
-            // Return shorter version of object
-            const asYaml = stringify(value);
-            const asJSON = JSON.stringify(value);
-            if (tokenizer.encode(asYaml).length < tokenizer.encode(asJSON).length) {
-                return asYaml;
+            if (typeof value.toISOString == "function") {
+                return value.toISOString();
             } else {
-                return asJSON;
+                // Return shorter version of object
+                const asYaml = stringify(value);
+                const asJSON = JSON.stringify(value);
+                if (tokenizer.encode(asYaml).length < tokenizer.encode(asJSON).length) {
+                    return asYaml;
+                } else {
+                    return asJSON;
+                }
             }
         } else if (value === undefined || value === null) {
             return '';
